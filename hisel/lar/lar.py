096e73a9412a4d3bd0504322e74296249d1165d0
Claudio <122022571+claudio-tw@users.noreply.github.com>
2023-04-24 21:19:50 +0100

    Build and distribute - no cython (#11)
    * Extra compile args in setup.py
    
    * Dont use cython
    
    * No installation in tests
    
    * sort active features only if path is valid

---
+++ hisel/lar/lar.py
@@ -0,0 +1,98 @@
+# Least angle regression
+import numpy as np
+from scipy.sparse import lil_matrix
+
+DTYPEf = np.float64
+DTYPEi = np.int64
+
+
+def solve(
+        x: np.ndarray,
+        y: np.ndarray,
+        dim_z: int,
+):
+    """
+    Solves the problem argmin_beta 1/2||y-X*beta||_2^2  s.t. beta>=0.
+    The problem is solved using a modification of the Least Angle Regression
+    and Selection algorithm.
+    We used the a Python implementation of the Nonnegative LARS solver
+    written in MATLAB at http://orbit.dtu.dk/files/5618980/imm5523.zip
+    """
+
+    assert dim_z > 0
+    n, d = x.shape
+    xx: np.ndarray = x.T @ x
+    g: np.ndarray = x.T @ y
+    beta: np.ndarray = np.zeros((d, 1), dtype=DTYPEf)
+    gb: np.ndarray = np.zeros((d, 1), dtype=DTYPEf)
+    c: np.ndarray = np.array(g, copy=True)
+    gw: np.ndarray = np.zeros((d, 1), dtype=DTYPEf)
+    gamma: np.ndarray = np.zeros((d+1, 1), dtype=DTYPEf)
+    maxc: float
+    j: int
+    t: int
+    aj: int
+    ij: int
+
+    active: List[int] = []
+    indices: List[int] = list(range(d))
+    num_active: int = 0
+    lasso_cond: int
+
+    path = lil_matrix((3*d, d))
+
+    # first step
+    k = 0  # index for path
+    j = np.argmax(c)
+    maxc = np.amax(c)  # c[j]
+    active.append(j)
+    indices.remove(j)
+    num_active = 1
+
+    # iteration
+    while np.sum(c[active]) / num_active >= 1e-12 and num_active <= dim_z:
+        w, _, _, _ = np.linalg.lstsq(
+            x[:, active].T @  x[:, active],
+            np.ones((num_active, 1), dtype=DTYPEf),
+            rcond=None
+        )
+        gw = x.T @ x[:, active] @ w
+        gamma[:d - num_active] = (maxc - c[indices]) / \
+            (gw[active[0]] - gw[indices])
+
+        gamma[d - num_active: d] = -beta[active] / w
+        gamma[d] = c[active[0]] / gw[active[0]]
+
+        gamma[gamma <= 1e-12] = np.inf
+        t = np.argmin(gamma)
+        beta[active] = beta[active] + gamma[t] * w
+
+        if d - num_active <= t and t < d:
+            lasso_cond = 1
+            j = t - d + num_active
+            aj = active[j]
+            indices.append(aj)
+            active.remove(aj)
+        else:
+            lasso_cond = 0
+
+        gb = xx @ beta
+        c = g - gb
+        j = np.argmax(c[indices])
+        maxc = np.amax(c[indices])  # c[indices][j]
+        path[k, :] = beta
+        if lasso_cond == 0:
+            ij = indices[j]
+            active.append(ij)
+            indices.remove(ij)
+
+        # update number of active variables
+        num_active = len(active)
+        k += 1
+
+    if len(active) > dim_z:
+        active.pop()
+
+    lassopath = path[:k, :].toarray()
+
+    return active, lassopath
