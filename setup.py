541f4f5f4f2a275b7d2ebfa3850a7abf7b99da39
claudio <claudio.bellani@transferwise.com>
2023-04-24 11:51:44 +0100

    Extra compile args in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 #
 # setup()
 
+import platform
 import os
 import setuptools
 from distutils.core import setup
@@ -11,13 +12,26 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 
+
+if platform.system() == "Windows":
+    compile_extra_args = []
+    link_extra_args = []
+elif platform.system() == "Linux":
+    compile_extra_args = ["-O3"]
+    link_extra_args = ["-O3"]
+elif platform.system() == "Darwin":
+    compile_extra_args = ["-O3", "-std=c++11", "-mmacosx-version-min=10.9"]
+    link_extra_args = ["-O3", "-stdlib=libc++", "-mmacosx-version-min=10.9"]
+
 slash = os.path.sep
 extensions = [
     Extension(
         'hisel.lar.lar',
         [f'hisel{slash}lar{slash}lar.pyx'],
         language='c++',
-        include_dirs=['hisel/', numpy.get_include()]
+        include_dirs=['hisel/', numpy.get_include()],
+        extra_compile_args=compile_extra_args,
+        extra_link_args=link_extra_args,
     )
 ]
 
