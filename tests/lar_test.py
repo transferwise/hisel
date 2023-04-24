284adfb66b2cbea82feb4578a8c48b872fd9aa1d
Claudio <122022571+claudio-tw@users.noreply.github.com>
2023-04-24 18:13:13 +0100

    Sort features by decreasing relevance when testing implementation of least-angle regression (#10)

--- tests/lar_test.py
+++ tests/lar_test.py
@@ -68,7 +68,12 @@ class TestLar(unittest.TestCase):
 
     def _test(self, x, y, a, beta):
         print('\nTesting implementation of least-angle regression in `hisel`')
-        active, _ = lar.solve(x, y, a)
+        active, path = lar.solve(x, y, a)
+        active = sorted(
+            active,
+            key=lambda a: path[-1, a],
+            reverse=True
+        )
         nonactive = list(set(range(x.shape[1])).difference(set(active)))
         if use_pyhsiclasso:
             print('Using pyHSICLasso.nlars for reconciliation purposes')
