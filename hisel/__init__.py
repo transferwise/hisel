cc494bd8a4153e893b4fceac93de42609d502795
claudio <claudio.bellani@transferwise.com>
2023-08-03 15:02:40 +0100

    Prepare the repo for open-sourcing

--- hisel/__init__.py
+++ hisel/__init__.py
@@ -1,6 +1,1 @@
 from . import kernels, select, permutohedron, hsic, categorical,  feature_selection  # NOQA
-try:
-    import torch   # NOQA
-    from . import torchkernels  # NOQA
-except (ImportError, ModuleNotFoundError):
-    pass
